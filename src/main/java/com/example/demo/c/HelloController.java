3c47a879335b42bd4f6903078ad3dc93e40f66e1
laiyu <383209920@qq.com>
2020-02-28 15:54:40 +0800

    FC-Hello World

---
+++ src/main/java/com/example/demo/c/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo.c;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloController {
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable("name") Integer name, Model model){
+
+        return "hello world";
+    }
+}
